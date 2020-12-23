e60a3f96a8685f5987f757939e74572adc4e74d5
artem.malieiev <artem.malieiev@exclaimer.com>
2020-12-23 16:20:18 +0200

    feat: added photo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AuthComponent } from './auth/auth.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { CheaterService } from './cheater.service';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
-  declarations: [AppComponent, AuthComponent, GalleryComponent],
+  declarations: [AppComponent, AuthComponent, GalleryComponent, PhotoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [
     CheaterService,
